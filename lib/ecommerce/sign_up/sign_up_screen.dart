87839446fca4b8e206d81617c6c9c57237de6673
Mahmoud Abdelsamad <m.m.abdelsamad82@gmail.com>
2023-09-01 15:40:29 +0200

    mahmoud ubdate

--- lib/ecommerce/sign_up/sign_up_screen.dart
+++ lib/ecommerce/sign_up/sign_up_screen.dart
@@ -20,102 +20,105 @@ class _SignUpScreenState extends State<SignUpScreen> {
     var height = MediaQuery.sizeOf(context).height;
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: Container(
-        width: double.infinity,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    textAlign: TextAlign.start,
-                    "Welcome to Our \ngrocery hop",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          width: double.infinity,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      textAlign: TextAlign.start,
+                      "Welcome to Our \ngrocery hop",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 40,
-                    ),
-                    width: double.infinity,
-                    padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Column(
-                      children: [
-                        CustomTextField(
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 40,
+                      ),
+                      width: double.infinity,
+                      padding: EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Column(
+                        children: [
+                          CustomTextField(
+                              height: height,
+                              text: "Name",
+                              controller: _nameController),
+                          SizedBox(
+                            height: height * .03,
+                          ),
+                          CustomTextField(
+                              height: height,
+                              text: "Email",
+                              controller: _emailController),
+                          SizedBox(
+                            height: height * .03,
+                          ),
+                          CustomTextField(
                             height: height,
-                            text: "Name",
-                            controller: _nameController),
-                        SizedBox(
-                          height: height * .03,
-                        ),
-                        CustomTextField(
-                            height: height,
-                            text: "Email",
-                            controller: _emailController),
-                        SizedBox(
-                          height: height * .03,
-                        ),
-                        CustomTextField(
-                          height: height,
-                          text: "Password",
-                          controller: _passwordController,
-                          isPassword: true,
-                        ),
-                        SizedBox(
-                          height: height * .1,
-                        ),
-                        Row(
-                          children: [
-                            Text(
-                              "Sign Up",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                            text: "Password",
+                            controller: _passwordController,
+                            isPassword: true,
+                          ),
+                          SizedBox(
+                            height: height * .1,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Spacer(),
-                            Container(
-                              height: 50,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                color: Colors.green,
-                                borderRadius: BorderRadius.circular(12)
+                              Spacer(),
+                              Container(
+                                height: 50,
+                                width: 100,
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(12)
+                                ),
+                                child: Icon(Icons.arrow_forward_rounded,size: 35,color: Colors.white,),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: height * .02,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Alreday Have Account?"),
+                              SizedBox(width: 10,),
+                              InkWell(
+                                onTap: (){
+                                  Get.off(LoginScreen());
+                                },
+                                child: Text("Log in",style: TextStyle(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.bold,
+                                ),),
                               ),
-                              child: Icon(Icons.arrow_forward_rounded,size: 35,color: Colors.white,),
-                            )
-                          ],
-                        ),
-                        SizedBox(
-                          height: height * .02,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text("Alreday Have Account?"),
-                            SizedBox(width: 10,),
-                            InkWell(
-                              onTap: (){
-                                Get.off(LoginScreen());
-                              },
-                              child: Text("Log in",style: TextStyle(
-                                color: Colors.green,
-                                fontWeight: FontWeight.bold,
-                              ),),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
