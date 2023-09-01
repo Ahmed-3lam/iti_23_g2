cdf61122c57cab03ef53c81c21465656a8bb6297
EsraaMagedd <esraamaged21664@gmail.com>
2023-09-01 15:17:53 +0300

    Esraa added BottomNavigationBar

--- lib/splash/splash_screen.dart
+++ lib/splash/splash_screen.dart
@@ -11,7 +11,26 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SplashScreen(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: 'Saved Courses',
+          ),
+        ],
+        currentIndex: 0,
+        onTap: (int index) {
+          // Do something when a tab is tapped.
+        },
+      ),
     );
   }
 }
